76a2f592d449ef6879a697c10bbb70be617e2d28
Enzo CONTY <contact@enzoconty.dev>
2019-11-23 16:41:34 +0100

    Improved List

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -33,42 +33,27 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        child: Text(
-                          '${transactions[index].amount.toStringAsFixed(2)}€',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
+                  elevation: 2,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text('${transactions[index].amount}€')),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat('dd MMMM yyyy, H:mm')
-                                .format(transactions[index].date),
-                            style: TextStyle(
-                                fontSize: 12, color: Colors.grey.shade600),
-                          )
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.d()
+                          .add_MMM()
+                          .add_y()
+                          .format(transactions[index].date),
+                    ),
                   ),
                 );
               },
